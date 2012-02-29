aa3e0ed1989566056f75d5e5e4aea661d4890242
Brian Bragg <brian.c.bragg@gmail.com>
2012-02-29 16:37:42 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rental::Application.config.secret_token = 'e22cece9441996174050f1d16ee295ec5001f380f03f74400c2dbcbfdea93152bdc59e44b50f6acbd49d7241164c1297d4391556817998a59e6442c5a1f3f91d'
